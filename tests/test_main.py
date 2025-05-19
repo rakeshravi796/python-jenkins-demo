9f86527fb5f55cd3af52191daafc887e813114bf
rakeshravi796 <rakesh.ravi@minfytech.com>
2025-05-19 23:44:16 +0530

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,9 +6,11 @@ class TestMain(unittest.TestCase):
     def test_greet_default(self):
         self.assertEqual(greet(), "Hello, World!")
 
+    
     def test_greet_name(self):
         self.assertEqual(greet("Jenkins"), "Hello, Jenkins!")
 
+    
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
         self.assertEqual(add(-1, 1), 0)
